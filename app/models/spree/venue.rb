d66060f297cef4a75e33dbaf80b951716ba0e6de
Adnard-kod <andra.lally@gmail.com>
2015-06-15 18:05:04 -0500

    moved locator file to prevent double include error

--- app/models/spree/venue.rb
+++ app/models/spree/venue.rb
@@ -1,7 +1,7 @@
 class Spree::Venue < ActiveRecord::Base
   geocoded_by :address
   after_validation :geocode, :if => :address_changed?
-  # include SpreeLocationNearYou
+  include SpreeLocationNearYou
 end
 
 # Spree::Venue.by_distance_from_latlong(37.766340, -122.416815)
