cc0d8373b516fd9515602941408c376572335e1d
acreilly <acreilly3@gmail.com>
2015-06-16 12:08:30 -0400

    fixed concerns

--- app/models/spree/venue.rb
+++ app/models/spree/venue.rb
@@ -1,7 +1,7 @@
 class Spree::Venue < ActiveRecord::Base
+  include Spree::Concerns::Locator
   geocoded_by :address
   after_validation :geocode, :if => :address_changed?
-  include SpreeLocationNearYou
 end
 
 # Spree::Venue.by_distance_from_latlong(37.766340, -122.416815)
