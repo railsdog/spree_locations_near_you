999368dfef82a4fdd35906a6c7565febad565633
acreilly <acreilly3@gmail.com>
2015-06-16 12:45:32 -0400

    fixed end

--- app/models/spree/concerns/locator.rb
+++ app/models/spree/concerns/locator.rb
@@ -4,7 +4,7 @@ module Spree
       extend ActiveSupport::Concern
       puts "**************you got here"
       included do
-          scope :by_distance_from_latlong, -> (lat, lng) {
+        scope :by_distance_from_latlong, -> (lat, lng) {
         # ** NOTE: postgres earthdistance orders points as (lng,lat) **
         # This method signature prefers lat,lng because that's how mapping is colloquially discussed.
         # The PG point() constructure requires lng,lat though so the ordering appears inverted at first glance.
@@ -18,7 +18,8 @@ module Spree
         .where("#{table_name}.latitude IS NOT NULL")
         .where("#{table_name}.latitude IS NOT NULL")
         .order("#{miles_apart} ASC")
-      }
+        }
+      end
     end
   end
 end
