fe40dc422c5bacc0337c5cac1cdcbe5a9022cda0
Adnard-kod <andra.lally@gmail.com>
2015-06-16 13:57:35 -0500

    add routes for venue.

---
+++ app/controllers/spree/admin/venues_controller.rb
@@ -0,0 +1,28 @@
+module Spree
+  module Admin
+    class VenuesController < Spree::Admin::ResourceController
+
+      def index
+        binding.pry
+        # show all venues
+      end
+
+      def new
+        # form to create new venue
+      end
+
+      def edit
+        #edit venue
+      end
+
+      def create
+        # create new venue
+      end
+
+      def delete
+        # delete new venue
+      end
+
+    end 
+  end
+end
