d97cd870ca68e8d80613b17baee5756f2c77a48e
Adnard-kod <andra.lally@gmail.com>
2015-06-25 11:54:03 -0500

    change object in venues near by action

--- app/controllers/spree/venues_controller.rb
+++ app/controllers/spree/venues_controller.rb
@@ -3,15 +3,16 @@ module Spree
     def index
       @stores = Spree::Store.all
     end
-
+    # miles is a ghost field check result object.miles
     def venues_near_by
-      user_location = Spree::Venue.create(address: params[:zipcode])
-      if !user_location.nil?
+      user_location = Geocoder.coordinates(params[:zipcode])
+      if !user_location.address.blank?
         @venues_near_by = Spree::Venue.by_distance_from_latlong(user_location.latitude, user_location.longitude)
+        render "index"
       else
+        # flash error
         # what to do if geocoder does not find an addres?
       end
-      render "index"
     end
   end
 end
