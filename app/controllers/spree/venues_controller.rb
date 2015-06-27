cab2e7297b5ca03bda40a9b7450ddf652b4cfad4
Adnard-kod <andra.lally@gmail.com>
2015-06-27 12:25:09 -0500

    refactor everything

--- app/controllers/spree/venues_controller.rb
+++ app/controllers/spree/venues_controller.rb
@@ -5,6 +5,7 @@ module Spree
       user_location = Spree::Venue.create(address: params[:zipcode] ) 
 
       session[:location] = [user_location.latitude, user_location.longitude]
+      # session[:zipcode] = params[:zipcode]
       @venues = Spree::Venue.by_distance_from_latlong(user_location.latitude, user_location.longitude)
 
       respond_to do |format|
@@ -21,10 +22,8 @@ module Spree
 
         format.json do
           if user_location.present?
-            @venues_near_by = Spree::Venue.by_distance_from_latlong(session[:location])
-            @venues_near_by.select {|v| v.rank == "sliver" && v.miles < 50 }
-            @venues_near_by = @venues_near_by.select {|v| v.miles < 50 }
-          render json:{ venues: @venues_near_by, user_location: user_location}
+            @venues_near_by = @venues.limit(5).select {|v| v.miles < 50 }
+            render json:{ venues: @venues_near_by, user_location: session[:location]}
           else
             render json:{ message: "There are no stores within 50 miles", venues: nil}
           end
@@ -32,37 +31,36 @@ module Spree
       end
     end
 
+    def drop_pins_on_load
+      if session[:location].present?
+        user_location = Spree::Venue.create(address: session[:location] )
+        @venues = Spree::Venue.by_distance_from_latlong(user_location.latitude, user_location.longitude)
+        @venues_near_by = @venues.limit(5).select {|v| v.miles < 50 }
+        render json:{ venues: @venues_near_by }
+      end
+    end
+
     def fliter_venues_near_by
       venues_list = []
-      user_location = Geocoder.coordinates(session[:user_location])
+      user_location = session[:location]
       venues = Spree::Venue.by_distance_from_latlong(user_location[0], user_location[1])
-
       if params[:sliver].present?
          sliver_near_by = venues.select {|v| v.rank == "sliver" && v.miles < 51}
-         sliver_near_by.present? if venues_list+= sliver_near_by
+         sliver_near_by.present? if venues_list += sliver_near_by
       end
       if params[:gold].present?
          gold_near_by = venues.select {|v| v.rank == "gold" && v.miles < 51}
-         gold_near_by.present? if venues_list+= gold_near_by
+         gold_near_by.present? if venues_list += gold_near_by
       end
       if params[:platinum].present?
          platinum_near_by = venues.select {|v| v.rank == "platinum" && v.miles < 51}
-         platinum_near_by.present? if venues_list+= platinum_near_by
+         platinum_near_by.present? if venues_list += platinum_near_by
       end
       if venues_list.present?
         render json:{ venues: venues_list, user_location: user_location}
       else 
         render json:{ message: "There are no stores within 50 miles", user_location: user_location, venues: nil}
       end
-    end
-
-    protected
-
-    def set_user_location(location)
-      session[:user_location] = location
-    end
-    def user_location
-      session[:user_location]
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Spree::Core::Engine.routes.draw do
     resources :venues
   end
   get "/venues/fliter_venues_near_by", to: "venues#fliter_venues_near_by", as: "fliter_venues_near_by"
+  get "/venues/drop_pins_on_load", to: "venues#drop_pins_on_load", as: "drop_pins_on_load"
   resources :venues, only: [:index]
 end
