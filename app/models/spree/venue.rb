96543b6b8403a9358e81b048cac003bd9a3e8eb8
acreilly <acreilly3@gmail.com>
2015-07-01 19:18:49 -0400

    adding validations

--- app/models/spree/venue.rb
+++ app/models/spree/venue.rb
@@ -3,9 +3,19 @@ class Spree::Venue < ActiveRecord::Base
   geocoded_by :address
   after_validation :geocode, :if => :address_changed?
   validates :name, presence: true
+  validates :street_address, presence: true
+  validates :city, presence: true
+  validates :zip, presence: true
+  validate :website_protocol_present
 
   def set_full_address
     self.address = "#{street_address} #{city} #{state} #{zip} #{country}"
     self.save
+  end
+
+  def website_protocol_present
+    unless website.include?("http://") || website.include?("https://")
+      errors.add(:website, 'Please add correct protocol. Example: http:// or https://')
+    end
   end
 end
