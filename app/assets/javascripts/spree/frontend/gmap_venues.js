a19036cc6144ae19a2bacf68dbdb73da827752d0
acreilly <acreilly3@gmail.com>
2015-07-01 17:32:46 -0400

    adding custom marker ability

--- app/assets/javascripts/spree/frontend/gmap_venues.js
+++ app/assets/javascripts/spree/frontend/gmap_venues.js
@@ -2,116 +2,126 @@
 
 $(document).ready(function(){
   if ($("#map")) {
-  function GmapVenues () {
-    this.venuesSearch = $("#searchVenues");
-    this.rank = $(".rank");
-    this.venuesNearBy =$("#venues-near-by")
-    this.location = JSON.parse($("#locationSession").html());
-    this.map = null;
-  };
-  GmapVenues.prototype = {
-  init: function(){
-    this.bindEvents();
-    this.checkRank();
-    this.checkDefaultStore()
-   if (this.location) {
-      this.setMap(this.location[0], this.location[1]);
-    }
-      this.setPinsOnload();
-  },
-  checkRank: function(){
-    $('.rank:input:checkbox').attr('checked', 'checked')
-  },
-  recheckRank: function(){
-    $(".rank-checkboxes label").addClass("checked")
-    $(".rank-checkboxes label").removeClass("unchecked")
-  },
-  bindEvents: function(){
-    this.venuesSearch.on('submit', function(e){
-      this.getVenues(e);
-    }.bind(this))
-    this.rank.on("click", function(e){
-      this.getFliteredVenues(e)
-    }.bind(this))
-    $(".venue-box input[type='checkbox']").on("click", this.setDefaultStore)
-  },
-  getVenues: function (e){
-    e.preventDefault();
-    var zip = $(e.target).children().first().val();
-    if(zip ){
-      $.ajax({
-         url: "/venues.json",
-         type: 'GET',
-         data: $(e.target).serialize()
-       })
-      .success(function(data){
-        this.recheckRank();
-        if(data.venues.length > 0){
-          this.setMap(data.user_location[0], data.user_location[1])
-          this.createPins(data.venues);
-          this.venuesTemplate(data.venues);
-        } else if (!data.user_location == "") {
-          this.setMap(data.user_location[0], data.user_location[1])
-          this.resetVenues()
-          this.venuesNearBy.append("<div class='no-store'>There are no stores in your area.</div>")
+    function GmapVenues () {
+      this.venuesSearch = $("#searchVenues");
+      this.rank = $(".rank");
+      this.venuesNearBy =$("#venues-near-by")
+      this.location = JSON.parse($("#locationSession").html());
+      this.map = null;
+    };
+    GmapVenues.prototype = {
+      init: function(){
+        this.bindEvents();
+        this.checkRank();
+        this.checkDefaultStore()
+        if (this.location) {
+          this.setMap(this.location[0], this.location[1]);
         }
-      }.bind(this))
-      .error(function(xhr) {
-        console.log("error something happened with retrieving your venues near you ")
-      }.bind())
-      }
-    },
-    setPinsOnload: function(){
-      $.ajax({
+        this.setPinsOnload();
+      },
+      checkRank: function(){
+        $('.rank:input:checkbox').attr('checked', 'checked')
+      },
+      recheckRank: function(){
+        $(".rank-checkboxes label").addClass("checked")
+        $(".rank-checkboxes label").removeClass("unchecked")
+      },
+      bindEvents: function(){
+        this.venuesSearch.on('submit', function(e){
+          this.getVenues(e);
+        }.bind(this))
+        this.rank.on("click", function(e){
+          this.getFliteredVenues(e)
+        }.bind(this))
+        $(".venue-box input[type='checkbox']").on("click", this.setDefaultStore)
+      },
+      getVenues: function (e){
+        e.preventDefault();
+        var zip = $(e.target).children().first().val();
+        if(zip ){
+          $.ajax({
+           url: "/venues.json",
+           type: 'GET',
+           data: $(e.target).serialize()
+         })
+          .success(function(data){
+            this.recheckRank();
+            if(data.venues.length > 0){
+              this.setMap(data.user_location[0], data.user_location[1])
+              this.createPins(data.venues);
+              this.venuesTemplate(data.venues);
+            } else if (!data.user_location == "") {
+              this.setMap(data.user_location[0], data.user_location[1])
+              this.resetVenues()
+              this.venuesNearBy.append("<div class='no-store'>There are no stores in your area.</div>")
+            }
+          }.bind(this))
+          .error(function(xhr) {
+            console.log("error something happened with retrieving your venues near you ")
+          }.bind())
+        }
+      },
+      setPinsOnload: function(){
+        $.ajax({
          url: "/venues/drop_pins_on_load",
          type: 'GET',
        })
-      .success(function(data){
-        if(data.venues.length > 0){
-          this.createPins(data.venues);
-        }
-      }.bind(this))
-      .error(function(xhr) {
-        console.log("error something happened with retrieving your venues near you ")
-      }.bind())
-    },
-    getFliteredVenues: function (e){
-      this.venuesNearBy.html('')
-      $.ajax({
+        .success(function(data){
+          if(data.venues.length > 0){
+            this.createPins(data.venues);
+          }
+        }.bind(this))
+        .error(function(xhr) {
+          console.log("error something happened with retrieving your venues near you ")
+        }.bind())
+      },
+      getFliteredVenues: function (e){
+        this.venuesNearBy.html('')
+        $.ajax({
          url: "/venues/fliter_venues_near_by",
          type: 'GET',
          data: $('input:checkbox:checked').serialize()
        })
-      .success(function(data){
-        map.removeMarkers();
-        if(data.venues.length > 0) {
-          this.createPins(data.venues)
-          this.venuesTemplate(data.venues)
-          this.checkDefaultStore()
-        $(".venue-box input[type='checkbox']").on("click", this.setDefaultStore)
-        }
-      }.bind(this))
-      .error(function(xhr) {
-        console.log("error something happened with retrieving your venues near you ")
-      }.bind())
-    },
-    setMap: function(lat, lng){
-      if (this.location) {
+        .success(function(data){
+          map.removeMarkers();
+          if(data.venues.length > 0) {
+            this.createPins(data.venues)
+            this.venuesTemplate(data.venues)
+            this.checkDefaultStore()
+            $(".venue-box input[type='checkbox']").on("click", this.setDefaultStore)
+          }
+        }.bind(this))
+        .error(function(xhr) {
+          console.log("error something happened with retrieving your venues near you ")
+        }.bind())
+      },
+      setMap: function(lat, lng){
+        if (this.location) {
          map = new GMaps({
           el: '#map',
           lat: lat ,
           lng: lng,
           zoom: 10
         });
-        map.removeMarkers();
-      };
-    },
-    setPin: function (lat, lng, name) {
-      map.addMarker({
-       lat: lat,
-       lng: lng,
-       title: 'New marker'
-      });
+         map.removeMarkers();
+       };
+     },
+     setPin: function (lat, lng, name) {
+      if(custom_marker_path != ""){
+        map.addMarker({
+         lat: lat,
+         lng: lng,
+         title: 'New marker',
+         icon: custom_marker_path
+       });
+
+      } else {
+        map.addMarker({
+         lat: lat,
+         lng: lng,
+         title: 'New marker'
+       });
+      }
     },
     createPins: function(data) {
       for (var i = data.length - 1; i >= 0; i--) {
@@ -145,7 +155,7 @@ $(document).ready(function(){
     }
   }
      // start
-   var venuesController = new GmapVenues();
-   venuesController.init();
-  };
-})
+     var venuesController = new GmapVenues();
+     venuesController.init();
+   };
+ })
--- lib/spree_locations_near_you/engine.rb
+++ lib/spree_locations_near_you/engine.rb
@@ -9,6 +9,12 @@ module SpreeLocationsNearYou
       g.test_framework :rspec
     end
 
+    initializer "spree.locations_near_you.preferences", :before => :load_config_initializers do |app|
+      Spree::AppConfiguration.class_eval do
+        preference :custom_marker_path, :string
+      end
+    end
+
     def self.activate
       Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
