e9b0d8bddda08624516b088c6e9f3d85a55fb80e
acreilly <acreilly3@gmail.com>
2015-07-02 15:58:14 -0400

    commenting out validations until we get the issue fixed

--- app/models/spree/venue.rb
+++ app/models/spree/venue.rb
@@ -3,10 +3,10 @@ class Spree::Venue < ActiveRecord::Base
   geocoded_by :address
   after_validation :geocode, :if => :address_changed?
   validates :name, presence: true
-  validates :street_address, presence: true
-  validates :city, presence: true
-  validates :zip, presence: true
-  validate :website_protocol_present
+  # validates :street_address, presence: true
+  # validates :city, presence: true
+  # validates :zip, presence: true
+  # validate :website_protocol_present
 
   def set_full_address
     self.address = "#{street_address} #{city} #{state} #{zip} #{country}"
