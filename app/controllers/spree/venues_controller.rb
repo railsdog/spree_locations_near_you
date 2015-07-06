2fd7f46fe3ac1178f9baea1b1e6554a1da9151ee
Daniel J. Pritchett <dpritchett@gmail.com>
2015-07-06 12:05:49 -0500

    Strip 50 mile radius

--- app/controllers/spree/venues_controller.rb
+++ app/controllers/spree/venues_controller.rb
@@ -19,21 +19,21 @@ module Spree
       respond_to do |format|
         format.html do
           if user_location.present?
-            @venues_near_by = @venues.limit(5).select {|v| v.miles < 50 }
+            @venues_near_by = @venues.limit(5)
             user_location.destroy
           render :index
           else
             #come back and do something here.
-            # render json:{ message: "There are no stores within 50 miles"}
+            # render json:{ message: "There are no stores available" }
           end
         end
 
         format.json do
           if user_location.present?
-            @venues_near_by = @venues.limit(5).select {|v| v.miles < 50 }
+            @venues_near_by = @venues.limit(5)
             render json:{ venues: @venues_near_by, user_location: session[:location]}
           else
-            render json:{ message: "There are no stores within 50 miles", venues: @venues_near_by}
+            render json:{ message: "There are no stores available.", venues: @venues_near_by}
           end
         end
       end
@@ -43,7 +43,7 @@ module Spree
       if session[:location].present?
         user_location = Spree::Venue.create(address: session[:location] )
         @venues = Spree::Venue.by_distance_from_latlong(user_location.latitude, user_location.longitude)
-        @venues_near_by = @venues.limit(5).select {|v| v.miles < 50 }
+        @venues_near_by = @venues.limit(5)
         render json:{ venues: @venues_near_by }
       end
     end
@@ -55,14 +55,14 @@ module Spree
 
       # Specific to G&G
       if params[:rank].present?
-         venues_near_by = venues.select {|v| params[:rank].keys.include?(v.rank) && v.miles < 51}
+         venues_near_by = venues.select {|v| params[:rank].keys.include?(v.rank) }
          venues_near_by.present? if venues_list += venues_near_by
       end
 
       if venues_list.present?
         render json:{ venues: venues_list, user_location: user_location}
       else
-        render json:{ message: "There are no stores within 50 miles", user_location: user_location, venues: venues_list}
+        render json:{ message: "There are no stores available", user_location: user_location, venues: venues_list}
       end
     end
   end
