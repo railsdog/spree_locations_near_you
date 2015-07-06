4ede689713f12b9f6677b5f952f438fccfca2854
Daniel J. Pritchett <dpritchett@gmail.com>
2015-07-06 12:14:12 -0500

    Make result limit configurable via instance method

--- app/controllers/spree/venues_controller.rb
+++ app/controllers/spree/venues_controller.rb
@@ -13,13 +13,13 @@ module Spree
         location = Spree::Venue.create(address: "10005" )
         session[:location] = [location.latitude, location.longitude]
       else
-        @venues_near_by = Spree::Venue.all.limit(5)
+        @venues_near_by = Spree::Venue.all.limit(max_results_returned)
       end
 
       respond_to do |format|
         format.html do
           if user_location.present?
-            @venues_near_by = @venues.limit(5)
+            @venues_near_by = @venues.limit(max_results_returned)
             user_location.destroy
           render :index
           else
@@ -30,7 +30,7 @@ module Spree
 
         format.json do
           if user_location.present?
-            @venues_near_by = @venues.limit(5)
+            @venues_near_by = @venues.limit(max_results_returned)
             render json:{ venues: @venues_near_by, user_location: session[:location]}
           else
             render json:{ message: "There are no stores available.", venues: @venues_near_by}
@@ -43,7 +43,7 @@ module Spree
       if session[:location].present?
         user_location = Spree::Venue.create(address: session[:location] )
         @venues = Spree::Venue.by_distance_from_latlong(user_location.latitude, user_location.longitude)
-        @venues_near_by = @venues.limit(5)
+        @venues_near_by = @venues.limit(max_results_returned)
         render json:{ venues: @venues_near_by }
       end
     end
@@ -64,6 +64,11 @@ module Spree
       else
         render json:{ message: "There are no stores available", user_location: user_location, venues: venues_list}
       end
+    end
+
+    private
+    def max_results_returned
+      5
     end
   end
 end
